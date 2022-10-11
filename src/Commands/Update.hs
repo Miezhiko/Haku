a72411b9f2855909fa625efa58039d3c646866c2
Miezhiko <Miezhiko@gmail.com>
2022-10-11 15:18:17 +0400

    update -u
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/Commands/Update.hs
+++ src/Commands/Update.hs
@@ -4,20 +4,41 @@ module Commands.Update where
 import           Types
 import           Utils
 
-update ∷ IO ()
-update = do
+import           Control.Monad
+
+data UpdateState
+  = UpdateState
+      { updUpgrade :: Bool
+      , updMinimal :: Bool
+      }
+
+updateOpts ∷ Bool → [OptDescr (UpdateState → UpdateState)]
+updateOpts _ =
+    [ Option "u" ["upgrade"] (NoArg (\s -> s { updUpgrade = True })) "run upgrade after"
+    ]
+
+update ∷ UpdateState → IO ()
+update upds = do
   runIfExists "/usr/bin/shelter"    "shelter" []
   runIfExists "/usr/bin/emerge"     "emerge" ["--sync"]
   runIfExists "/usr/bin/egencache"  "egencache" ["--repo=gentoo", "--update"]
   runIfExists "/usr/bin/eix-update" "eix-update" []
+  when (updUpgrade upds) $
+    rawAndIgnore "emerge" [ "-avuDN"
+                          , "@world"
+                          , "--backtrack=100"
+                          , "--with-bdeps=y"
+                          , "--quiet-build=n"
+                          ]
 
-updateCmd ∷ Command String
+updateCmd ∷ Command UpdateState
 updateCmd = Command
               {
                 command = ["update"],
                 description = "Update world",
                 usage = ("haku " ++),
-                state = [],
-                options = const [],
-                handler = \_ _ _ -> update
+                state = UpdateState { updUpgrade = False
+                                    , updMinimal = False },
+                options = updateOpts,
+                handler = \_ upds _ -> update upds
               }
