af4aea558360985f1a8d692a7a425c1950fc5004
Miezhiko <Miezhiko@gmail.com>
2023-02-14 12:43:28 +0400

    don't store mask boolean in version data
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/Portage/Version.hs
+++ src/Portage/Version.hs
@@ -18,14 +18,14 @@ getVersion overlay pn ebuild = do
       version   = case parseVersion ver of
                     Left   _ → error $ "getVersion: version parse error '" ++ ver ++ "'"
                     Right  x →  x
-  PackageVersion version overlay False False
+  PackageVersion version overlay False
 
 getVersionInstalled ∷ String → String → String → Either String PackageVersion
 getVersionInstalled overlay pn path =
   let ver = drop (length pn + 1) path
   in case parseVersion ver of
       Left   _ → Left  $ "getVersionInstalled: version parse error '" ++ ver ++ "'"
-      Right  x → Right $ PackageVersion x overlay True False
+      Right  x → Right $ PackageVersion x overlay True
 
 isLiveVersion ∷ Version → Bool
 isLiveVersion (Version ver _ _ _ _) = 9999 `elem` ver
