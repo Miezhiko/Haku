5657c71857ca241a71de82c729ee6c4ad2746ff1
Miezhiko <Miezhiko@gmail.com>
2023-03-16 15:41:30 +0400

    ability to show updates without masks and only masks
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/Commands/Updates.hs
+++ src/Commands/Updates.hs
@@ -19,6 +19,20 @@ import qualified Data.Set            as S
 
 import           System.Console.ANSI
 
+data UpdatesState
+  = UpdatesState
+      { updsVerbose   :: Bool
+      , updsShowMasks :: Bool
+      , updsOnlyMasks :: Bool
+      }
+
+updatesOpts ∷ Bool -> [OptDescr (UpdatesState -> UpdatesState)]
+updatesOpts _ =
+    [ Option "v" ["verbose"] (NoArg (\s -> s { updsVerbose = True })) "verbose output"
+    , Option "m" ["show-masks"] (NoArg (\s -> s { updsShowMasks = True })) "show masked"
+    , Option "o" ["only-masks"] (NoArg (\s -> s { updsOnlyMasks = True })) "show only masked"
+    ]
+
 {-
 showMod DNONE =  ""
 showMod DLT   =  "<"
@@ -37,8 +51,8 @@ isVersionMasked v (DepAtom _neg _rev DGT _ _ (DepVer vv _ast) _slt)  = v > vv
 isVersionMasked _ (DepAtom _neg _rev _ _ _ _ _)                      = True
 
 printWithMasks ∷ [DepAtom] -> PackageVersion -> IO ()
-printWithMasks masks (PackageVersion v ov installed) = do
-  let findMask = find (isVersionMasked v) masks
+printWithMasks mask (PackageVersion v ov installed) = do
+  let findMask = find (isVersionMasked v) mask
   case findMask of
     Nothing -> setSGR [ SetColor Foreground Vivid Blue ]
     _       -> setSGR [ SetColor Foreground Vivid Red ]
@@ -50,9 +64,11 @@ printWithMasks masks (PackageVersion v ov installed) = do
                   Just m  -> " [Masked " ++ show m ++ "]"
                   Nothing -> 𝜀
 
-prettyShowVersionsListAndMasking ∷ [DepAtom] -> [PackageVersion] -> IO ()
-prettyShowVersionsListAndMasking _ [] = pure ()
-prettyShowVersionsListAndMasking masks vers = do
+prettyPrintVersionsListAndMasking ∷ [DepAtom]
+                                 -> [PackageVersion]
+                                 -> IO ()
+prettyPrintVersionsListAndMasking _ [] = pure ()
+prettyPrintVersionsListAndMasking masks vers = do
   setSGR [ SetColor Foreground Vivid Blue ]
   putStr "Available:"
   for_ vers (printWithMasks masks)
@@ -72,40 +88,75 @@ checkForEbuild pc p ver =
                       then pure $ Just ver -- good keywords
                       else pure Nothing    -- bad keywords
 
-showOnlyInstalled ∷ Package
-                -> [PackageVersion]
-                -> [PackageVersion]
-                -> [DepAtom]
-                -> PortageConfig
-                -> IO ()
-showOnlyInstalled _ _ [] _ _      = pure ()
-showOnlyInstalled _ [] _ _ _      = pure ()
-showOnlyInstalled p [x] vv mask pc =
+prettyPrintUpdates ∷ Package
+                  -> PackageVersion
+                  -> [DepAtom]
+                  -> [PackageVersion]
+                  -> IO ()
+prettyPrintUpdates p x mask new = do
+  setSGR [ SetConsoleIntensity BoldIntensity
+        , SetUnderlining SingleUnderline ]
+  print p
+  setSGR [ SetColor Foreground Vivid Green
+        , SetUnderlining NoUnderline
+        , SetConsoleIntensity BoldIntensity ]
+  putStrLn $ prettyShowVersionsList [x]
+  prettyPrintVersionsListAndMasking mask new
+  setSGR [ Reset ]
+  putStrLn []
+
+checkedPrintUpdates ∷ Package
+                   -> PackageVersion
+                   -> [DepAtom]
+                   -> [PackageVersion]
+                   -> Bool
+                   -> IO ()
+checkedPrintUpdates p x mask new displayMasks =
+  let notMasked = if displayMasks
+      then filter (\pv -> any ( isVersionMasked (pvVersion pv)
+                              ) mask
+                  ) new
+      else filter (\pv -> not $ any ( isVersionMasked (pvVersion pv)
+                                    ) mask
+                  ) new
+  in case notMasked of
+    [] -> pure ()
+    xs -> prettyPrintUpdates p x mask xs
+
+printOnlyInstalled ∷ Package
+                 -> [PackageVersion]
+                 -> [PackageVersion]
+                 -> [DepAtom]
+                 -> PortageConfig
+                 -> UpdatesState
+                 -> IO ()
+printOnlyInstalled _ _ [] _ _ _      = pure ()
+printOnlyInstalled _ [] _ _ _ _      = pure ()
+printOnlyInstalled p [x] vv mask pc uss =
   let higherVersions = filter (> x) vv
   in unless (null higherVersions) $ do
-    newVersionsWithEbuilds <-
-      catMaybes <$> traverse (checkForEbuild pc p) higherVersions
-    unless (null newVersionsWithEbuilds) $ do
-      setSGR [ SetConsoleIntensity BoldIntensity
-            , SetUnderlining SingleUnderline ]
-      print p
-      setSGR [ SetColor Foreground Vivid Green
-            , SetUnderlining NoUnderline
-            , SetConsoleIntensity BoldIntensity ]
-      putStrLn $ prettyShowVersionsList [x]
-      prettyShowVersionsListAndMasking mask newVersionsWithEbuilds
-      setSGR [ Reset ]
-      putStrLn []
-showOnlyInstalled p xs vv mask pc =
+    new <- catMaybes <$> traverse (checkForEbuild pc p) higherVersions
+    unless (null new) $
+      if updsShowMasks uss
+        then prettyPrintUpdates p x mask new
+        else if updsOnlyMasks uss
+          then checkedPrintUpdates p x mask new True
+          else checkedPrintUpdates p x mask new False
+printOnlyInstalled p xs vv mask pc uss =
   let maxInstalledVersion = maximum xs
-  in showOnlyInstalled p [maxInstalledVersion] vv mask pc
+  in printOnlyInstalled p [maxInstalledVersion] vv mask pc uss
 
 filteredDep ∷ String -> String -> DepAtom -> Bool
 filteredDep pcat pname (DepAtom _ _ _ cat name _ _) = 
   cat == pcat && pname == name
 
-showSingle ∷ Package -> [OverlayMeta] -> MaskingData -> PortageConfig -> IO ()
-showSingle package ovs mask pc =
+showSingle ∷ Package
+          -> [OverlayMeta]
+          -> MaskingData
+          -> PortageConfig
+          -> UpdatesState
+          -> IO ()
+showSingle package ovs mask pc uss =
   let versions      = pVersions package
       versionsList  = S.toList versions
       installed     = filter pvInstalled versionsList
@@ -123,27 +174,29 @@ showSingle package ovs mask pc =
           xs -> let ovMasks = concatMap (\(_, od) -> ovMasking od) xs
                 in map mDepAtom (ovMasks ++ pMask mask)
       finalMasks = filter (filteredDep category name) masksForPkg
-  in showOnlyInstalled package installed notInstalled finalMasks pc
+  in printOnlyInstalled package installed notInstalled finalMasks pc uss
 
-showU ∷ IORef PortageConfig -> [String] -> IO ()
-showU rpc filterPackages = readIORef rpc >>= \pc ->
+showU ∷ IORef PortageConfig -> UpdatesState -> [String] -> IO ()
+showU rpc uss filterPackages = readIORef rpc >>= \pc ->
   let tree = pcTree pc
       mask = pcMasking pc
       ovls = M.toList (pcOverlays pc)
   in for_ (M.toList tree) $ \(a, package) ->
     case filterPackages of
-      [] -> showSingle package ovls mask pc
-      xs -> when (any (`isInfixOf` a) xs) $ showSingle package ovls mask pc
+      [] -> showSingle package ovls mask pc uss
+      xs -> when (any (`isInfixOf` a) xs) $ showSingle package ovls mask pc uss
 
-showPossibleUpdates ∷ HakuMonad m ⇒ String -> [String] -> m ()
-showPossibleUpdates _ xs = ask >>= \env ->
-   liftIO $ showU (config env) xs
+showPossibleUpdates ∷ HakuMonad m ⇒ UpdatesState -> [String] -> m ()
+showPossibleUpdates uss xs = ask >>= \env ->
+   liftIO $ showU (config env) uss xs
 
-updatesCmd ∷ Command String m
+updatesCmd ∷ Command UpdatesState m
 updatesCmd = Command
             { command = ["updates"]
             , description = "Show possible updates"
             , usage = ("haku " ++)
-            , state = 𝜀
-            , options = const 𝜀
+            , state = UpdatesState { updsVerbose    = False
+                                   , updsShowMasks  = False
+                                   , updsOnlyMasks  = False }
+            , options = updatesOpts
             , handler = showPossibleUpdates }
